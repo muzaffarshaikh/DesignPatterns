e81587b70d7094af3c658c2737e2f97a04e24e20
muzaffarshaikh <52539616+muzaffarshaikh@users.noreply.github.com>
2020-11-11 14:55:35 +0530

    Chain of Responsibility Pattern Added

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -1,6 +1,6 @@
 package builder;
 
-public class TheBuilderPatternDemo {
+public class Main {
     
     public static void main(String[] args) {
         
---
+++ src/cor/Main.java
@@ -0,0 +1,32 @@
+package cor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Main {
+    
+    //Chain Of Responsibility
+
+    public static void main(String[] args) {
+        PurchaseOrder po;
+        President p = new President();
+        VicePresident vp = new VicePresident();
+        Secretary s = new Secretary();
+        
+        s.setSuccessor(vp);
+        vp.setSuccessor(p);
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Order ID : ");
+        int oid = sc.nextInt();
+        System.out.println("Enter Amount : ");
+        double amt = sc.nextDouble();
+        
+        po = new PurchaseOrder(oid, amt);
+        
+        s.sanctionMoney(po);
+    }
+}
