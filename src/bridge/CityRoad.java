1d997a2c19b129fe9b679a1fcadbe05eac3394dd
muzaffarshaikh <52539616+muzaffarshaikh@users.noreply.github.com>
2020-11-11 14:41:14 +0530

    Bridge Pattern Added

---
+++ src/bridge/CityRoad.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridge;
+
+/**
+ *
+ * @author Student
+ */
+  class CityRoad extends Road {
+ 
+      public void DisplayMileage(){
+          this.mileage = 7;
+          
+          System.out.println("The Mileage on City Road is : " +mileage);
+      }
+              
+}
---
+++ src/bridge/Maruti800.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridge;
+
+/**
+ *
+ * @author Student
+ */
+ public class Maruti800 extends MarutiCar{
+
+    
+    @Override
+    public void DisplayMileage() {
+         
+        CityRoad c = new CityRoad();
+        Highway h = new Highway();
+        
+        
+        this.NoOfSeats =5;
+        this.Year=2000;
+        
+        System.out.println("No Of Seats:" +NoOfSeats);
+        
+        System.out.println("Year:" +Year);
+        
+        c.DisplayMileage();
+        
+        h.DisplayMileage();
+    }
+
+    
+       
+        
+    
+
+    
+     
+    
+  
+      
+       
+        
+    }
+     
+
